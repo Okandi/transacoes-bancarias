2b2c27ad410624868953e883006c681cea7f29a1
Edu <edu.b.longhi@gmail.com>
2022-11-16 10:18:36 -0300

    reformulação modelo csv

--- src/repository/csv_file/tb_csv_file_writer.py
+++ src/repository/csv_file/tb_csv_file_writer.py
@@ -1,7 +1,6 @@
 from src.infrastructure.csv_file.tb_csv_file_infrastructure import TBCSVFileInfrastructure
 from src.core.interfaces.csv_file_interface.itb_csv_file_writer import ITBCSVFileWriter
 from src.core.models.consolidador_info import ConsolidadorInfo
-import json
 
 
 class TBCSVFileWriter(ITBCSVFileWriter):
@@ -10,11 +9,7 @@ class TBCSVFileWriter(ITBCSVFileWriter):
         self._csv_file = TBCSVFileInfrastructure()
 
     def save_transacao(self, info: ConsolidadorInfo) -> None:
-        transacao = {
-            "tipo": info.tipo.name,
-            "valor": info.valor,
-            "envio": str(info.envio)
-        }
+        transacao = f"{info.tipo.name},{info.valor},{str(info.envio)}"
         with self._csv_file.get_file_access() as file:
-            file.write(json.dumps(transacao))
             file.write("\n")
+            file.write(transacao)
